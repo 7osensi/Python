0eb77f06dee67d6ef86f085455d11fde8c334663
7osensi <husseinmhosny6@gmail.com>
2023-08-12 13:07:27 +0300

    Check if the character is vowel

---
+++ 01/CheckVowel.py
@@ -0,0 +1,10 @@
+character = input("Enter a characters : ")
+
+# List of vowels
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+# Check if the character is in the list
+if character in vowels:
+    print(True) 
+else:
+    print(False)
