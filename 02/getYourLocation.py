9aef148c828690bb696e28c98a74c4793983adb9
7osensi <husseinmhosny6@gmail.com>
2023-09-05 22:57:45 +0300

    Used ipapo inted of ipinfo because it's not opening

--- 02/getYourLocation.py
+++ 02/getYourLocation.py
@@ -1,21 +1,27 @@
 from urllib.request import urlopen
-
 import webbrowser
-
 import json
 
-# store the URL in url variable as parameter for urlopen
-url = "https://api.ipify.org/?format=json"
+# # store the URL in url variable as parameter for urlopen
+# ip_url = "https://api.ipify.org/?format=json"
+
+# # store the response of URL
+# response1 = urlopen(ip_url)
+
+# # Storing the JSON response from url and store it in ip
+# ip = json.loads(response1.read())
+
+# print(ip['ip'])
+
+location_url = "https://ipapi.co/json/"
 
 # store the response of URL
-response = urlopen(url)
+response2 = urlopen(location_url)
 
-# storing the JSON response from url in data
-data_json = json.loads(response.read())
+# Storing the JSON response from url and store it in ip
+location = json.loads(response2.read())
 
-# print the json response
-print(data_json)
-
-site = "https://ipinfo.io/data_json/geo"
-
-webbrowser.open(site)
+# print the json response (ip)
+print(location['ip'])
+print(location['country_name'])
+print(location['city'])
